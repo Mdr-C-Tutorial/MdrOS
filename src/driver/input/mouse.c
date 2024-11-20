cd3723a733ab5585f935802796a9b331395fc110
XIAOYI12 <www18845340923@163.com>
2024-11-20 23:53:49 +0800

    更新鼠标驱动

--- src/driver/input/mouse.c
+++ src/driver/input/mouse.c
@@ -13,14 +13,14 @@ MOUSE_DEC mdec;
 int mouse_x;
 int mouse_y;
 
-static void kb_wait(){
+static void kb_wait() {
     uint8_t kb_stat;
     do {
         kb_stat = io_in8(KB_CMD);
     } while (kb_stat & 0x02);
 }
 
-static bool mouse_decode(uint8_t data){
+static bool mouse_decode(uint8_t data) {
     if (mdec.phase == 0) {
         if (data == 0xfa) {
             mdec.phase = 1;
@@ -56,7 +56,8 @@ static bool mouse_decode(uint8_t data){
 
 static void mouse_handler(registers_t *reg) {
     uint8_t data = io_in8(KB_DATA);
-    if(mouse_decode(data)){
+
+    if (mouse_decode(data)) {
         mouse_x += mdec.x;
         mouse_y += mdec.y;
         if (mouse_x < 0) mouse_x = 0;
@@ -64,14 +65,27 @@ static void mouse_handler(registers_t *reg) {
         if (mouse_x > get_vbe_width() - 1) mouse_x = get_vbe_width() - 1;
         if (mouse_y > get_vbe_height() - 1) mouse_y = get_vbe_height() - 1;
 
+        mdec.left = false;
+        mdec.center = false;
+        mdec.right = false;
+
+        if(mdec.btn & 0x01){
+            mdec.left = true;
+        }
+        if(mdec.btn & 0x02){
+            mdec.right = true;
+        }
+        if(mdec.btn & 0x04) {
+            mdec.center = true;
+        }
     }
 }
 
-int get_mouse_x(){
+int get_mouse_x() {
     return mouse_x;
 }
 
-int get_mouse_y(){
+int get_mouse_y() {
     return mouse_y;
 }
 
@@ -90,6 +104,7 @@ void mouse_init() {
 
     mouse_x = (get_vbe_width() - 16) / 2;
     mouse_y = (get_vbe_height() - 28 - 29) / 2;
+    mdec.phase = 0;
 
-    klogf(true,"Load PS/2 Mouse device.\n");
+    klogf(true, "Load PS/2 Mouse device.\n");
 }
